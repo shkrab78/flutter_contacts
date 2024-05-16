8fb0f08d0a82c3359d72df5aade09869c8a40887
Андрей <shkrab78@gmail.com>
2024-05-16 17:07:19 +0300

    update bug fix

--- android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
+++ android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
@@ -287,16 +287,19 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
           return true;
         }
         Uri contactUri = intent.getData();
-          if (intent != null){
-        Cursor cursor = contentResolver.query(contactUri, null, null, null, null);
-        if (cursor.moveToFirst()) {
-          String id = contactUri.getLastPathSegment();
-          getContacts("openDeviceContactPicker", id, false, false, false, localizedLabels, this.result);
+        if (intent != null) {
+          Cursor cursor = contentResolver.query(contactUri, null, null, null, null);
+          if (cursor.moveToFirst()) {
+            String id = contactUri.getLastPathSegment();
+            getContacts("openDeviceContactPicker", id, false, false, false, localizedLabels, this.result);
+          } else {
+            Log.e(LOG_TAG, "onActivityResult - cursor.moveToFirst() returns false");
+            finishWithResult(FORM_OPERATION_CANCELED);
+          }
+          cursor.close();
         } else {
-          Log.e(LOG_TAG, "onActivityResult - cursor.moveToFirst() returns false");
-          finishWithResult(FORM_OPERATION_CANCELED);
-        }}else{return true;}
-        cursor.close();
+          return true;
+        }
         return true;
       }
 
